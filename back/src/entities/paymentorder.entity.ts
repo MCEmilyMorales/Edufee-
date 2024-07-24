b3b4f4427bdf2ec228a85842ab17b776405c96fc
Luciano Badenas <163030721+Lbadenas@users.noreply.github.com>
2024-07-24 15:39:10 -0300

    entidades

---
+++ back/src/entities/paymentorder.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'paymentOrder' })
+export class PaymentOrder {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  date: Date;
+}
+
+// una vez que hablemos con el profesor vamos a crear las relaciones
+//estudiantes id
