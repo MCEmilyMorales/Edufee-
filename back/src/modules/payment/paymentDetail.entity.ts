3b64a6e2a60d6acbfe67d926b03822655bd858fa
Emily Morales <15ruth05@gmail.com>
2024-07-24 18:29:28 -0300

    relacion entity

---
+++ back/src/modules/payment/paymentDetail.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
+import { PaymentOrder } from '../payment-order/paymentOrder.entity';
+
+@Entity({ name: 'paymentDetails' })
+export class PaymentDetail {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  fecha: Date;
+
+  @OneToOne(()=>PaymentOrder,(paymentOrder) => paymentOrder.id)
+  paymentOrder_id: PaymentOrder
+}
