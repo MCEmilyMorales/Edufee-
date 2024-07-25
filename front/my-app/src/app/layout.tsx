907403c7b0981a7d0c36c1e7624ae252189957ff
Juan Martinez HATZU <jmartinez96mol@gmail.com>
2024-07-25 18:08:45 -0500

    raleway

--- front/my-app/src/app/layout.tsx
+++ front/my-app/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Raleway } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
 const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="${raleway.className}${inter.className}">
         <Navbar />
         {children}
         <Footer />
