2aa99aafe984f2daf469324f279c3bbaaf81e2f4
Bruno Distaulo <0800bruno03@gmail.com>
2024-07-27 21:12:31 -0300

    footer avanzado

--- front/my-app/src/components/footer/footer.tsx
+++ front/my-app/src/components/footer/footer.tsx
@@ -3,12 +3,19 @@ import BotonLink from "../botonLink/boton";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full bg-[#C9EBFF] fixed bottom-0 text-white h-24 flex items-center">
-      <ul className="flex flex-col gap-0 pl-4 text-black">
-        <BotonLink link="/" text="placeholder" />
-        <BotonLink link="/b" text="placeholder" />
-        <BotonLink link="/c" text="placeholder" />
-      </ul>
+    <footer className="w-full bg-[#C9EBFF] sticky text-black h-24 flex items-center justify-between p-6">
+      <div>
+        <p className="text-center font-bold text-lg font-sans">Contactanos a traves de:</p>
+        <p className="text-left font-sans">+569 12345678</p>
+        <a href="mailto:p5i9b@example.com" className="text-left">Alguien@example.com</a>
+      </div>
+      <div>
+        <p className="text-center text-lg"><b className="font-sans">© 2024 Edufee. </b>Todos los derechos reservados.</p>
+      </div>
+      <div className="h-24">
+        <p className="text-center font-bold text-lg pt-2">¿Donde nos encontramos?</p>
+        <p className="text-center pt-4">Calle <b className="font-sans font-normal">123</b>, San Francisco, <b className="font-sans font-normal">94111</b></p>
+      </div>
     </footer>
   );
 };
