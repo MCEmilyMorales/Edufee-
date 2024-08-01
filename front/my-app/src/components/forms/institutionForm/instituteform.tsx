93b4d213f875d1ddd30f1a41da30a31e16cf11be
Diego Marin <paolo05061@gmail.com>
2024-08-01 18:47:37 -0300

    Formularios controlados y listo para las peticiones al backend

--- front/my-app/src/components/forms/institutionForm/instituteform.tsx
+++ front/my-app/src/components/forms/institutionForm/instituteform.tsx
@@ -1,61 +1,115 @@
-import Link from "next/link";
-import React from "react";
+'use client'
 
-const instituteForm: React.FC = () => {
+import React from "react";
+import FormInput from "@/components/FormInputInstitution";
+import { FormDataInstitute, useFormInstitute } from "@/hooks/useFormInstitute";
+import { useRouter } from "next/navigation";
+import { registerInstitution } from "@/helpers/institution.helper";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+const InstituteForm: React.FC = () => {
+  const initialState: FormDataInstitute = {
+    nombreInstitucion: "",
+    direccion: "",
+    telefono: "",
+    numeroCuenta: "",
+    email: "",
+    logo: "",
+    banner: "",
+  };
+  const router = useRouter();
+  const { formData, errors, handleChange, validate } = useFormInstitute(initialState);
+  const { user} = useUser();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    
+    if (validate()) {
+      formData.email = user?.email!;
+
+      try {
+        const response = await registerInstitution(formData);
+        console.log("Respuesta del servidor:", response);
+        router.push("/institucion")   
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-8">
       <div className="w-full max-w-md p-8 bg-[#FFDA16] border-2 border-black shadow-lg rounded-[2em] flex flex-col items-center">
         <h2 className="text-2xl font-bold mb-4">Registro de Institución</h2>
-        <form className="w-full space-y-4">
-          <input
+        <form className="w-full space-y-4" onSubmit={handleSubmit}>
+          <FormInput
             type="text"
+            name="nombreInstitucion"
             placeholder="Nombre de la Institución"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            value={formData.nombreInstitucion}
+            onChange={handleChange}
+            error={errors.nombreInstitucion}
           />
-          <input
+          <FormInput
             type="text"
+            name="direccion"
             placeholder="Dirección"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            value={formData.direccion}
+            onChange={handleChange}
+            error={errors.direccion}
           />
-          <input
+          <FormInput
             type="text"
+            name="telefono"
             placeholder="Teléfono"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            value={formData.telefono}
+            onChange={handleChange}
+            error={errors.telefono}
           />
-          <input
+          <FormInput
             type="text"
-            placeholder="Numero de cuenta"
-            className="w-full p-3 border border-gray-300 rounded-md"
+            name="numeroCuenta"
+            placeholder="Número de cuenta"
+            value={formData.numeroCuenta}
+            onChange={handleChange}
+            error={errors.numeroCuenta}
           />
-          <label htmlFor="logo" className="mt-2 block">
-            Subir Logo
-          </label>
-          <input
-            id="logo"
-            type="file"
-            className="w-full p-3 border bg-gray-300 border-gray-300 rounded-md"
-            placeholder="Subir Logo"
-          />
-
-          <label htmlFor="banner" className="mt-2 block">
-            Banner
-          </label>
-          <input
-            id="banner"
-            type="file"
-            className="w-full p-3 border bg-gray-300 border-gray-300 rounded-md"
-            placeholder="Subir banner"
-          />
-            <button
-              type="submit"
-              className="w-full mt-4 p-3 bg-[#16ABFF] border-2 border-black text-white rounded-md shadow-lg hover:bg-[#1657FF] transition-colors duration-300"
-            >
-              Registrar
-            </button>
+          <div>
+            <label htmlFor="logo" className="mt-2 block">
+              Subir Logo
+            </label>
+            <input
+              id="logo"
+              type="file"
+              name="logo"
+              className="w-full p-3 border bg-gray-300 border-gray-300 rounded-md"
+              onChange={handleChange}
+            />
+            {errors.logo && <p className="text-red-500">Debes subir el logo de la institución</p>}
+          </div>
+          <div>
+            <label htmlFor="banner" className="mt-2 block">
+              Subir Banner
+            </label>
+            <input
+              id="banner"
+              type="file"
+              name="banner"
+              className="w-full p-3 border bg-gray-300 border-gray-300 rounded-md"
+              onChange={handleChange}
+            />
+            {errors.banner && <p className="text-red-500">Debes subir el banner de la institución</p>}
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-4 p-3 bg-[#16ABFF] border-2 border-black text-white rounded-md shadow-lg hover:bg-[#1657FF] transition-colors duration-300"
+          >
+            Registrar
+          </button>
         </form>
       </div>
     </div>
   );
 };
 
-export default instituteForm;
+export default InstituteForm;
