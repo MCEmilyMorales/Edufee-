bd39f0caf0c23cdcc17143d98d12ff4f5967093e
Juan Martinez HATZU <jmartinez96mol@gmail.com>
2024-08-02 12:31:17 -0500

    stripe cosmetic routes on select

--- front/my-app/src/app/payment-success/page.tsx
+++ front/my-app/src/app/payment-success/page.tsx
@@ -4,13 +4,16 @@ export default function PaymentSuccess({
   searchParams: { amount: string };
 }) {
   return (
-    <main className="relative max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
-      <div className="mt-32">
-        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-        <h2 className="text-2xl">You successfully sent</h2>
+    <main
+      className="relative overflow-auto font-inter h-screen flex flex-col items-center  space-y-8 text-white text-center border
+     bg-gradient-to-tr from-blue-500 to-purple-500 pb-32"
+    >
+      <div className="mt-32 p-4">
+        <h1 className="text-4xl font-extrabold mb-2">Gracias</h1>
+        <h2 className="text-2xl">Pago enviado a ISTITUCION A</h2>
 
         <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
-          ${amount}
+          Institucion A por: ${amount}
         </div>
       </div>
     </main>
