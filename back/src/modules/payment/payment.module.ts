6a5940a7d961b0aa009f3d62b608e5e6b8e89f57
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-08-05 12:40:01 -0300

    payments CRUD

--- back/src/modules/payment/payment.module.ts
+++ back/src/modules/payment/payment.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentDetailController } from './payment.controller';
-import { PaymentDetailService } from './payment.service';
+import { PaymentService } from './payment.service';
+import { PaymentsRepository } from './payment.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './payment.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment])],
   controllers: [PaymentDetailController],
-  providers: [PaymentDetailService],
+  providers: [PaymentService, PaymentsRepository],
 })
-export class PaymentDetailModule {}
+export class PaymentModule {}
--- back/src/modules/users/users.entity.ts
+++ back/src/modules/users/users.entity.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm';
 import { Institution } from '../institution/institution.entity';
 import { ApiProperty } from '@nestjs/swagger';
-import { PaymentOrder } from '../payment-order/paymentOrder.entity';
+import { Payment } from '../payment/payment.entity';
 
 @Entity({ name: 'users' })
 export class User {
@@ -91,6 +91,6 @@ export class User {
   @ManyToOne(() => Institution, (institution) => institution.user_id)
   institution: Institution;
 
-  @OneToMany(() => PaymentOrder, (paymentOrder) => paymentOrder.user)
-  paymentOrder: PaymentOrder;
+  @OneToMany(() => Payment, (payment) => payment.pdfImage)
+  payments: Payment[];
 }
