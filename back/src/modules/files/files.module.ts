8d5506efce8d71b1536682a543a26ccf379a0c0c
Luciano Badenas <163030721+Lbadenas@users.noreply.github.com>
2024-08-05 22:44:54 -0300

    post de prueba para ver que me mandan

--- back/src/modules/files/files.module.ts
+++ back/src/modules/files/files.module.ts
@@ -1,30 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FilesUserService } from './filesUser.service';
 import { FilesUserController } from './filesUser.controller';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/users.entity';
-import { CloudinaryConfig } from 'src/config/cloudinary';
 import { Institution } from '../institution/institution.entity';
 import { FilesInstitutionService } from './filesInstitution.service';
 import { FilesInstitutionController } from './filesInsitution.controller';
-import { filePaymentController } from './filespaymentController';
-import { FilePaymentService } from './filepaymentService';
+
 import { FilesRepository } from './files.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Institution])],
-  providers: [
-    FilesUserService,
-    FilesInstitutionService,
-    FilePaymentService,
-    FilesRepository,
-    CloudinaryConfig,
-  ],
-  controllers: [
-    FilesUserController,
-    FilesInstitutionController,
-    filePaymentController,
-  ],
+  providers: [FilesUserService, FilesInstitutionService, FilesRepository],
+  controllers: [FilesUserController, FilesInstitutionController],
 })
 export class FilesModule {}
