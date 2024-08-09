6460221fe3f46df47917b1b659c82f2a4d85cb30
Bruno Distaulo <0800bruno03@gmail.com>
2024-08-08 23:24:03 -0300

    middleware para la dash

--- front/my-app/src/middleware.ts
+++ front/my-app/src/middleware.ts
@@ -6,16 +6,14 @@ const jwtpassword = process.env.JWT_SECRET || 'secret';
 export async function middleware(req: NextRequest) {
   const cook = req.cookies.get('authToken');
   const token = cook?.value as string; 
-  console.log("esto es el token del middleware", token)
 
   if (!token) {
     return NextResponse.redirect(new URL('/api/auth/login', req.url));
   }
 
   try {
-    const decodedToken: any = jwt.verify(token!, jwtpassword);
-    console.log("esto llega del decodedToken", decodedToken)
-    if (decodedToken.role !== 'admin') {
+    const payload = JSON.parse(atob(token.split(".")[1]))
+    if (payload.roles.includes('student') || payload.roles.includes('institution')) {  
       return NextResponse.redirect(new URL('/verify-user', req.url));
     }
   } catch (error) {
